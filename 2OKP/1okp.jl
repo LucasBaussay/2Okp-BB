152f33e95d2c269abdb87a90d1f9feed062d18fd
jnicolasthouvenin <jules.nicolasthouvenin@gmail.com>
2021-03-08 17:09:46 +0100

    commments on 1OKP

--- 2OKP/1okp.jl
+++ 2OKP/1okp.jl
@@ -81,10 +81,12 @@ end
     return the unique solution of the given 1OKP
 
     @prob : 1OKP to solve (the sub problems have the same number of variables)
-    @assignment : array of assignments (of length n)
-    @indFinAssignment : index of the last assigned variable
+    @assignment : array of assignments (of length n) (non ordered)
+    @indFinAssignment : index of the last assigned variable (non ordered)
     @assignmentWeight : the sum of the weights of the variables assigned to one
     @assignmentProfit : the sum of the profits of the variables assigned to one
+
+    @bestSolPrim : current best primal solution (indexed on ordered variables) : bestSolPrim[1] targets the first best variable.
 """
 function solve1OKP(prob::Problem, assignment::Vector{Int} = Vector{Int}(), indFinAssignment::Int = 0, assignmentWeight::Float64 = 0., assignmentProfit::Float64 = 0.; verbose = false)
     @assert prob.nbObj == 1 "This problem is no 1OKP"
@@ -108,52 +110,62 @@ function solve1OKP(prob::Problem, assignment::Vector{Int} = Vector{Int}(), indFi
         revPermList[permList[iter]] = iter
     end
 
-    # 
+    # weight still available
     poidsRestPrim = prob.constraint.maxWeight - assignmentWeight
-    bestSolPrim = zeros(Bool, prob.nbVar) #Par Objets triés
 
-    iterLastOne = 0
-
-    for iter = 1:prob.nbVar
-        if assignment[permList[iter]] != -1
-            bestSolPrim[iter] = assignment[permList[iter]]
-            if assignment[permList[iter]] == 1
-                iterLastOne = iter
+    # current best primal solution (indexed on ordered variables) : bestSolPrim[1] targets the first best variable.
+    bestSolPrim = zeros(Bool, prob.nbVar)
+    # construction of bestSolPrim
+    iterLastOne = 0 # index (ordered) of the last variable assigned to one
+    for iter = 1:prob.nbVar # iterating on variables non ordered
+        var = permList[iter]
+        if assignment[var] != -1 # if the variable is assigned
+            bestSolPrim[iter] = assignment[var] # the current sol assign this var to the same value
+            if assignment[var] == 1 # the variable is assigned to one
+                iterLastOne = iter # index (ordered) of the last variable assigned to one
             end
         end
     end
 
-    iter = indFinAssignment + 1
+    iter = indFinAssignment + 1 # index of the first non assigned variable
 
-    lb = 0. + assignmentProfit
+    lb = 0. + assignmentProfit # profit we are sure to make because the solution is feasible
     ub = Inf
 
     """
 
-    Initialisation pour avoir une LowerBound et UpperBound
+    Construction of a good first feasible solution being our LowerBound. We calculate our UpperBound by adding a portion of the next item.
 
     """
 
+    # goal : inside bestSolPrim, we want to force to one all the first variables (ordered) that could fit in the knapsack
+    # iterating on the variables after iter. We stop if we can't assign the next variable to one. 
     while iter <= prob.nbVar && prob.constraint.weights[permList[iter]] <= poidsRestPrim
-        bestSolPrim[iter] = 1
-        poidsRestPrim -= prob.constraint.weights[permList[iter]]
-        lb += prob.objs[1].profits[permList[iter]]
-        iterLastOne = iter
+        var = permList[iter] # index of the variable (non ordered)
+        bestSolPrim[iter] = 1 # we put the variable to one in the current solution
+        poidsRestPrim -= prob.constraint.weights[var] # we substract the weight of the variable that we assigned to one 
+        lb += prob.objs[1].profits[var] # we add to the primal bound the profit of var
+        iterLastOne = iter # update iterLastOne
         iter += 1
     end
 
-    if iter <= prob.nbVar
+    # goal : check if bestSolPrim is optimal, and if not, calculate a first upper bound
+    if iter <= prob.nbVar # at least one object is assigned to zero
+        # we can set the upper bound by adding the portion of the next item that can fit inside the knapsack
         ub = lb + (poidsRestPrim / prob.constraint.weights[permList[iter]]) * prob.objs[1].profits[permList[iter]]
-    else
+    else # all the variables are assigned to one
+        # bestSolPrim is optimal
         return Solution(bestSolPrim[revPermList], [lb])
     end
 
-    while iter <= prob.nbVar && poidsRestPrim != 0
-        if prob.constraint.weights[permList[iter]] <= poidsRestPrim
-            bestSolPrim[iter] = 1
-            poidsRestPrim -= prob.constraint.weights[permList[iter]]
-            lb += prob.objs[1].profits[permList[iter]]
-            iterLastOne = iter
+    # goal : we finish to go through variables and try to put as many to one.
+    # iterating on remaining variables (non ordered), we stop if we don't have any space remaining in the bag
+    while iter <= prob.nbVar && poidsRestPrim > 0
+        if prob.constraint.weights[permList[iter]] <= poidsRestPrim # if the current var feets in the bag
+            bestSolPrim[iter] = 1 # assigning the var to one
+            poidsRestPrim -= prob.constraint.weights[permList[iter]] # updating the remaining space in the bag
+            lb += prob.objs[1].profits[permList[iter]] # updating the lower bound because we added a variable
+            iterLastOne = iter # updating iterLastOne
         end
         iter += 1
     end
@@ -164,7 +176,7 @@ function solve1OKP(prob::Problem, assignment::Vector{Int} = Vector{Int}(), indFi
 
     """
 
-    verbose && println("On comment le Backtracking")
+    verbose && println("On commence le Backtracking")
 
     if iterLastOne > indFinAssignment
         sol, lb = backtrack(prob, indFinAssignment, permList, iterLastOne, bestSolPrim, lb, poidsRestPrim, lb, ub, verbose = verbose)
