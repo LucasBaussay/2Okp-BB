bf66f59a54d79077ea08a0ea032380aa6cb912d5
jnicolasthouvenin <jules.nicolasthouvenin@gmail.com>
2021-03-08 16:31:32 +0100

    commments on 1OKP

--- 2OKP/1okp.jl
+++ 2OKP/1okp.jl
@@ -1,3 +1,6 @@
+
+"""Solves a 1OKP"""
+
 function backtrack(prob::Problem, indFinAssignment::Int, permList::Vector{Int}, iter::Int, bestSolPrim::Vector{Bool}, profitPrim::Float64, poidsRestPrim::Float64, lb::Float64, ub::Float64; verbose = false)
 
     if iter == indFinAssignment || lb == ub
@@ -72,9 +75,21 @@ function backtrack(prob::Problem, indFinAssignment::Int, permList::Vector{Int}, 
 
 end
 
+"""
+    solve1OKP(prob::Problem, assignment::Vector{Int} = Vector{Int}(), indFinAssignment::Int = 0, assignmentWeight::Float64 = 0., assignmentProfit::Float64 = 0.; verbose = false)
+    
+    return the unique solution of the given 1OKP
+
+    @prob : 1OKP to solve (the sub problems have the same number of variables)
+    @assignment : array of assignments (of length n)
+    @indFinAssignment : index of the last assigned variable
+    @assignmentWeight : the sum of the weights of the variables assigned to one
+    @assignmentProfit : the sum of the profits of the variables assigned to one
+"""
 function solve1OKP(prob::Problem, assignment::Vector{Int} = Vector{Int}(), indFinAssignment::Int = 0, assignmentWeight::Float64 = 0., assignmentProfit::Float64 = 0.; verbose = false)
     @assert prob.nbObj == 1 "This problem is no 1OKP"
 
+    # if the assignment is empty, we initialize the array at a size of nbVars in prob
     if assignment == []
         assignment  = Vector{Int}(undef, prob.nbVar)
         for iter = 1:prob.nbVar
@@ -82,14 +97,18 @@ function solve1OKP(prob::Problem, assignment::Vector{Int} = Vector{Int}(), indFi
         end
     end
 
+    # permutation list sorting the variables. Beginning with the best ones, ending with worsts.
+    # usage : permList[1] gives the index of the first best variable
     permList = sortperm(prob.objs[1].profits ./ prob.constraint.weights, rev = true)
 
+    # inverse permutation list allowing to get back to the original indexes of the variables
     revPermList = Vector{Int}(undef, length(permList))
-
+    # construction of the inverse permutation list
     for iter = 1:length(permList)
         revPermList[permList[iter]] = iter
     end
 
+    # 
     poidsRestPrim = prob.constraint.maxWeight - assignmentWeight
     bestSolPrim = zeros(Bool, prob.nbVar) #Par Objets triés
 
