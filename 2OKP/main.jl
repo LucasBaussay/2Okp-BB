24c55f9d5c873e5f5babcb1fa7d541b6486d4505
Lucas Baussay <lulubaussay@gmail.com>
2021-03-18 12:42:29 +0100

    Just : It works

--- 2OKP/main.jl
+++ 2OKP/main.jl
@@ -31,17 +31,23 @@ function solve1Okp(prob::Problem, id::Int = -1; verbose = false)
     @assert prob.nbObj == 1 "This problem is no 1OKP"
     weightSum = sum(prob.constraint.weights)
 
+    resPretrait = true
+    for iter = 1:prob.nbVar
+        resPretrait = resPretrait && prob.constraint.weights[iter] > prob.constraint.maxWeight
+    end
+
     if weightSum < prob.constraint.maxWeight
         return Solution(trues(prob.nbVar), [sum(prob.objs[1].profits)], weightSum, id)
+    elseif resPretrait
+        return Solution(falses(prob.nbVar), [0], 0, id)
     else
-
-        verbose && println("On solve avec l'algo de Léane : $prob")
+        # verbose && println("On solve avec l'algo de Léane : $prob")
 
         ListObj = [Objet(prob.objs[1].profits[iter], prob.constraint.weights[iter], iter) for iter = 1:prob.nbVar]
 
         sol = main1Okp(ListObj, prob.constraint.maxWeight)
 
-        verbose && println("On obtient : $sol")
+        # verbose && println("On obtient : $sol")
 
         return Solution(Bool.(sol.X), [sol.z], sum(sol.X .* prob.constraint.weights), id)
     end
@@ -213,6 +219,8 @@ function computeLowerBound!(lowerBound::Vector{Solution}, consecutiveSet::Vector
     prob = subProb(mainProb, assignment)
     verbose && println("Le problème auxiliaire équivaut à $prob ")
 
+    assignment.indEndAssignment == 0 ? assignment = Assignment(Vector{Int}(), 0, zeros(Int, mainProb.nbObj), 0, Vector{PairOfSolution}()) : nothing
+
     S = Vector{Tuple{Solution, Solution}}()
 
     # computing the two first points, sol1 is the best for the only the objective number 1, and sol2 for the objective number 2.
@@ -223,7 +231,7 @@ function computeLowerBound!(lowerBound::Vector{Solution}, consecutiveSet::Vector
     sol1 = evaluate(prob, sol1.x)
     sol2 = evaluate(prob, sol2.x)
 
-    verbose && println("On crée les solution lexico : $(sol1.y) et $(sol2.y)")
+    verbose && println("On crée les solution lexico : $(sol1.y .+ assignment.profit) et $(sol2.y .+ assignment.profit)")
 
     if sol1.y == sol2.y # solutions are identicals
         push!(lowerBound, createSuperSol(sol1, assignment))
@@ -237,7 +245,7 @@ function computeLowerBound!(lowerBound::Vector{Solution}, consecutiveSet::Vector
         while length(S) != 0 # while we have solutions to study
             solR, solL = pop!(S) # get the two consecutive points from the queue of tuples to study
 
-            verbose && println("On étudie la paire de solution : $(solR.y) et $(solL.y)")
+            verbose && println("On étudie la paire de solution : $(solR.y .+ assignment.profit) et $(solL.y .+ assignment.profit)")
             # computing the direction of the search
             λ = [solL.y[2]-solR.y[2], solR.y[1]-solL.y[1]]
             # computing the resulting solution
@@ -247,7 +255,7 @@ function computeLowerBound!(lowerBound::Vector{Solution}, consecutiveSet::Vector
             if sum(λ .* solE.y) > sum(λ .* solR.y) # solE is better than solR according to λ
                 push!(lowerBound, createSuperSol(solE, assignment)) # solE is solution we want
                 push!(S, (solR, solE), (solE, solL)) # now we need to study (solR, solE) and (solE, solL)
-                verbose && println("On a trouvé la solution : $(solE.y)")
+                verbose && println("On a trouvé la solution : $(solE.y .+ assignment.profit)")
             else # solE is equal to solR according to λ, so we didn't find new solutions
                 push!(consecutiveSet, PairOfSolution(createSuperSol(solR, assignment), createSuperSol(solL, assignment), length(consecutiveSet)+1)) # we know we won't find solutions between solR, solL, so we memorize the tuple
                 verbose && println("On a rien trouvé dans cette direction")
